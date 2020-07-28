0b9ec981de5e2d09b4bfd0a46b264aa3e2e32a67
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-07-28 12:29:36 -0300

    Padões de projeto

--- src/br/com/pagamentos/commandpattern/Cobranca.java
+++ src/br/com/pagamentos/commandpattern/Cobranca.java
@@ -9,5 +9,15 @@ public class Cobranca {
 	private double valor;
 	private String msg;
 	
+	public void abrir() {
+        this.estado = Estado  .ABERTO;
+        System.out.println("O portão abriu...");
+    }
+
+    public void fechar() {
+        this.estado = EstadoPortao.FECHADO;
+        System.out.println("O portão fechou...");
+    }
+	
 	
 }
--- src/br/com/pagamentos/commandpattern/CobrarAmigo.java
+++ src/br/com/pagamentos/commandpattern/CobrarAmigo.java
@@ -1,5 +1,17 @@
 package br.com.pagamentos.commandpattern;
 
-public class CobrarAmigo {
+public class CobrarAmigo implements Comando {
+
+	@Override
+	public String fazer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String desfazer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
